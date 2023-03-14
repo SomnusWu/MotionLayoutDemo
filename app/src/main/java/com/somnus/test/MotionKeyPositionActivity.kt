0d15926d3c0458ceab700d1aa9077b4e0dd5d4bc
Somnus <wuzhensomnus@163.com>
2023-03-14 11:49:42 +0800

    url

--- app/src/main/java/com/somnus/test/MotionKeyPositionActivity.kt
+++ app/src/main/java/com/somnus/test/MotionKeyPositionActivity.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
  *     https://juejin.cn/post/6854573206653812743#heading-4
  *     https://juejin.cn/post/6854573206653812743#heading-3
  *
+ *
+ *     https://medium.com/google-developers/defining-motion-paths-in-motionlayout-6095b874d37#b4c1
+ *
  */
 class MotionKeyPositionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
